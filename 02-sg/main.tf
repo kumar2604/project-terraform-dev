f8894db635daa4136a8012f2448a192fcc8443d5
kumar <sivakumarlella26@email.com>
2024-10-17 12:10:03 +0530

    added vpc files

---
+++ 02-sg/main.tf
@@ -0,0 +1,29 @@
+module "db" {
+  source = "../../terraform-aws-securitygroup"
+  project_name = var.project_name
+  environment = var.environment
+  sg_description = "SG for DB MySQL Instances"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+  sg_name = "db"
+}
+
+module "backend" {
+  source = "../../terraform-aws-securitygroup"
+  project_name = var.project_name
+  environment = var.environment
+  sg_description = "SG for Backend Instances"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+  sg_name = "backend"
+}
+
+module "frontend" {
+  source = "../../terraform-aws-securitygroup"
+  project_name = var.project_name
+  environment = var.environment
+  sg_description = "SG for Frontend Instances"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+  sg_name = "frontend"
+}
